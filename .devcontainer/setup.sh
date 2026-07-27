170dd59994517499c146edcf95e0cf9e706d2eb3
Thunboo <53939052+Thunboo@users.noreply.github.com>
2026-07-27 04:04:35 +0300

    Began working on ansible auto-setup for vps
    Probably will make it a lab soon, but for now just for my own purposes

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+echo "source /home/vscode/.venv/ansible-dev/bin/activate" >> ~/.bashrc
+echo "cd /workspace/development" >> ~/.bashrc
+source ~/.bashrc
+ansible-galaxy collection install --force /workspace/collections/*.tar.gz /workspace/thunboo/postgresql_deploy 2>/dev/null
+pip install "molecule-plugins[docker]"
+# docker image inspect centos9_systemd || docker build -t centos9_systemd /workspace/thunboo/postgresql_deploy/roles/thunboo_postgres/molecule/default/docker_files
+# docker network inspect molecule_test_net || docker network create --subnet=100.10.10.0/24 molecule_test_net
